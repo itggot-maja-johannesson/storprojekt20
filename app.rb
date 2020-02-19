e2bd1b94707690933b472852bb67428bf364103c
itggot-maja-johannesson <60006132+itggot-maja-johannesson@users.noreply.github.com>
2020-02-19 20:39:55 +0100

    Added some routes and connected to the database

---
+++ app.rb
@@ -0,0 +1,46 @@
+require 'sinatra'
+require 'slim'
+require 'sqlite3'
+require 'bcrypt'
+enable :sessions
+
+def connect_to_db(path)
+    db = SQLite3::Database.new(path)
+    db.results_as_hash = true
+    return db
+end
+   
+# post('/users/') do                         Exempel + den övre för att få mindre kod
+#     db = connect_to_db('db/todo.db')
+#     username = params["username"]
+#     password = params["password"]
+#     db.execute("INSERT INTO users(username, password) VALUES (?,?)", [username, password])
+#     redirect('/photos')
+# end
+   
+
+get('/') do 
+    slim(:index)
+end
+
+get('/login') do
+    slim(:'users/index')
+end
+
+post('/login') do
+    db = connect_to_db('db/Blocket.db')
+    username = params["username"]
+    password = params["password"]
+
+    # db.execute("INSERT INTO users(username, password) VALUES (?,?)", [username, password])
+    redirect('/login')
+end
+
+get('/register') do
+    slim(:'users/new')
+end
+
+post('/register') do
+    db = connect_to_db('db/Blocket.db')
+    redirect('/login')
+end
