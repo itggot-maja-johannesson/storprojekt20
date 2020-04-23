d973bd8b6108bad11b69821a977d31bdeacf4444
itggot-maja-johannesson <60006132+itggot-maja-johannesson@users.noreply.github.com>
2020-04-23 15:44:19 +0200

    Started working on the 'add comment' route

--- app.rb
+++ app.rb
@@ -9,21 +9,37 @@ def connect_to_db(path)
     db.results_as_hash = true
     return db
 end
-   
-# post('/users/') do                         Exempel + den övre för att få mindre kod
-#     db = connect_to_db('db/todo.db')
-#     username = params["username"]
-#     password = params["password"]
-#     db.execute("INSERT INTO users(username, password) VALUES (?,?)", [username, password])
-#     redirect('/photos')
-# end
-   
-# before('/') do 
-#     session[:user_id] = 1
-# end
 
 get('/') do 
     slim(:index)
+end
+
+get('/register') do
+    slim(:'users/new')
+end
+
+post('/register') do
+    db = connect_to_db('db/Blocket.db')
+    db.results_as_hash = true
+    username = params["username"]
+    password = params["password"]
+    password_confirmation = params["confirm_password"]
+
+    result = db.execute("SELECT id FROM users WHERE username=?", username)
+
+    if result.empty? 
+        if password == password_confirmation
+            password_digest = BCrypt::Password.create(password)
+            db.execute("INSERT INTO users(username, password) VALUES (?,?)", [username, password_digest])
+            redirect('/postlayout')
+        else
+            session[:user_error] = "Passwords don't match!"
+            redirect('/register')
+        end
+    else
+        session[:user_error] = "Sorry, username already taken!"
+        redirect('/register')
+    end
 end
 
 get('/login') do
@@ -59,37 +75,10 @@ post('/login') do
     # redirect('/postlayout')
 end
 
-get('/register') do
-    slim(:'users/new')
+
+before('/postlayout') do 
+    session[:user_id] = 1
 end
-
-post('/register') do
-    db = connect_to_db('db/Blocket.db')
-    db.results_as_hash = true
-    username = params["username"]
-    password = params["password"]
-    password_confirmation = params["confirm_password"]
-
-    result = db.execute("SELECT id FROM users WHERE username=?", username)
-
-    if result.empty? 
-        if password == password_confirmation
-            password_digest = BCrypt::Password.create(password)
-            db.execute("INSERT INTO users(username, password) VALUES (?,?)", [username, password_digest])
-            redirect('/postlayout')
-        else
-            session[:user_error] = "Passwords don't match!"
-            redirect('/register')
-        end
-    else
-        session[:user_error] = "Sorry, username already taken!"
-        redirect('/register')
-    end
-end
-
-# before('/postlayout') do 
-#     session[:user_id] = 1
-# end
 
 
 get('/postlayout') do
@@ -97,30 +86,168 @@ get('/postlayout') do
     slim(:'Posts/new')
 end
 
-get('/postlayout') do
-    slim(:'Posts/show')
-end
-
-
 post('/postlayout') do 
     db = connect_to_db('db/Blocket.db')
     db.results_as_hash = true
 
-    p "-----------------------------"
+    p "-------outside!--------"
+    
+    case false
+        when !!session[:user_id]
+            session[:post_error] = "You need to be logged in"
 
-    p user_id = session[:user_id]
-    p title = params[:title]
-    p text = params[:specification]
-    p price = params[:price]
+        when !params[:title].empty?
+            session[:post_error] = "You need to have a title"
 
-    p "-----------------------------"
+        when !params[:specification].empty?
+            session[:post_error] = "You need to have a text"
 
-    p path = File.join("./public/uploaded_pictures/", params[:file][:filename])
-    File.write(path, File.read(params[:file][:tempfile]))
+        when !params[:price].empty?
+            session[:post_error] = "You need to add a price"
 
-    db.execute("INSERT INTO posts(user_id, title, text, picture_source, price) VALUES (?,?,?,?,?)", [user_id, title, text, path, price])
+        when !!params[:file]
+            session[:post_error] = "You need to add a picture"
+    end
+
+    unless session[:post_error]
+        p "---------inside!----------"
+
+        p user_id = session[:user_id]
+        p title = params[:title]
+        p text = params[:specification]
+        p price = params[:price]
+
+        params.to_a
+
+        p cat_ids = params.to_a.select { |param| param[0].include?("cat_") }
+        p cat_ids = cat_ids.map { |cat_id| cat_id = cat_id[0].split("").select.with_index { |_, i| i > 3 }.join.to_i }
+
+        p "---------inside!----------"
+
+        p path = File.join("./public/uploaded_pictures/", params[:file][:filename])
+        File.write(path, File.read(params[:file][:tempfile]))
+
+        db.execute("INSERT INTO posts(user_id, title, text, picture_source, price) VALUES (?,?,?,?,?)", [user_id, title, text, path, price])
+        p post_id = db.execute("SELECT id FROM posts WHERE user_id = ?", [user_id])[1]["id"]
+        cat_ids.each { |cat_id| db.execute("INSERT INTO posts_categories (post_id, categorie_id) VALUES (?,?)", [post_id, cat_id]) }
+    end
 
     redirect('/postlayout')
+end
+
+get('/showposts') do
+    slim(:'Posts/show')
+end
+
+# post('/showposts') do
+#     db = connect_to_db('db/Blocket.db')
+#     db.results_as_hash = true
+
+#     user_id = session[:user_id]
+#     title = params[:title]
+#     text = params[:specification]
+#     price = params[:price]
+
+#     post = db.execute("SELECT (title, text, price) FROM posts WHERE id=?", id)
+#     return post
+
+#     redirect('/postlayout')
+# end
+
+def get_post_from_id(id)
+    db = connect_to_db('db/Blocket.db')
+    
+    post = db.execute("SELECT (posts.title, posts.text, posts.price) FROM posts WHERE id=?", id)
+    return post
+end
+
+# ska kolla om personen har behörighet
+
+before do
+    session[:user_liked] = {}
+    session[:error] = ""
+    session[:user_id] = 1
+    if session[:user_id] == nil
+        case request.path_info
+        when '/'
+            break     
+        when '/sign_in'
+            break
+        when '/sign_in_user'
+            break
+        when '/create_user'
+            break
+        when '/sign_up'
+            break
+        when '/test'
+            break
+        else
+            session[:error] = "You need to be logged in order to do this"
+            redirect('/login')
+        end    
+    end
+end 
+
+get('/delete_post') do
+end
+
+post('/delete_post') do
+end
+
+get('/edit_post') do
+end
+
+post('/edit_post') do
+end
+
+get('/youraccount') do
+    slim(:'Users/show')
+end
+
+post('/youraccount') do
+    db = connect_to_db('db/Blocket.db')
+    username = params["username"]
+
+    redirect('/youraccount')
+end
+
+get('/add_comment') do
+    slim(:'Comments/new')
+end
+
+post('/add_comment') do
+    db = connect_to_db('db/Blocket.db')
+    db.results_as_hash = true
+    
+    case false
+        when !!session[:user_id]
+            session[:comment_error] = "You need to be logged in"
+
+        when !params[:text].empty?
+            session[:comment_error] = "You need to write a comment"
+    end
+
+    unless session[:comment_error]
+
+        p user_id = session[:user_id]
+        p text = params[:text]
+
+        params.to_a
+
+        File.write(path, File.read(params[:file][:tempfile]))
+
+        db.execute("INSERT INTO comments(user_id, text) VALUES (?,?)", [user_id, text])
+        p comment_id = db.execute("SELECT id FROM comments WHERE user_id = ?", [user_id])[1]["id"]
+        # något med post_id för att få till 'posts_comments'
+    end
+
+    redirect('/postlayout')
+end
+
+get('/delete_comment') do 
+end
+
+post('/delete_comment') do 
 end
 
 
